6ece3b9601bd7ba78b0ac55a70de6472f3252bbc
SaadAhmad381 <saadahmad1456@gmail.com>
2023-04-18 11:37:59 +0500

    adding product.css and className

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
+import "product.css";
 
 const Products = () => {
   const [data, setData] = useState([]);
@@ -15,9 +16,9 @@ const Products = () => {
   }, []);
 
   return (
-    <div>
+    <div className="products">
       {data.map((m) => (
-        <div className="api">
+        <div className="card">
           <img src={m.image} alt="no image" height="150px" width="150px" />
           <h5>{m.id}</h5>
           <p>{m.price}</p>
