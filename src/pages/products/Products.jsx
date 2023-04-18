59768d5862f5006ef803dbbb6deb516ac0564f3c
SaadAhmad381 <saadahmad1456@gmail.com>
2023-04-18 11:59:44 +0500

    add import

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
-import "product.css";
+import "./product.css";
 
 const Products = () => {
   const [data, setData] = useState([]);
